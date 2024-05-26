852f34ef8601bcf3716dbe740502f7ef1ef4f5fc
Billubeza <156518807+Billubeza@users.noreply.github.com>
2024-05-26 13:44:46 +0300

    Create circle.java

---
+++ circle.java
@@ -0,0 +1,61 @@
+public class Circle {
+        private int[] queue;
+        private int front;
+        private int rear;
+        private int capacity;
+    
+        public void CircularQueue(int k) {
+            capacity = k;
+            queue = new int[capacity];
+            front = -1;
+            rear = -1;
+        }
+    
+        public boolean enqueue(int value) {
+            if (isFull()) {
+                return false;
+            }
+            if (isEmpty()) {
+                front++;
+            }
+            rear = (rear + 1) % capacity;
+            queue[rear] = value;
+            return true;
+        }
+    
+        public boolean dequeue() {
+            if (isEmpty()) {
+                return false;
+            }
+            if (front == rear) {
+                front = -1;
+                rear = -1;
+            } else {
+                front = (front + 1) % capacity;
+            }
+            return true;
+        }
+    
+        public int front() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue[front];
+        }
+    
+        public int rear() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue[rear];
+        }
+    
+        public boolean isEmpty() {
+            return front == -1;
+        }
+    
+        public boolean isFull() {
+            return (rear + 1) % capacity == front;
+        }
+    }
+}
